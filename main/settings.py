5e9046f9abbfb26f41c3530bcaf9bddf0b8983b8
Amjad Projects <aakhan1572@yahoo.com>
2024-02-21 05:19:43 +0530

    ‘Main’

--- main/settings.py
+++ main/settings.py
@@ -12,7 +12,7 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False),
+    DEBUG=(bool, True),
 
 )
 
